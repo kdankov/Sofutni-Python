50c02ba6fa0f6ff4a5133ae6517d9db69d2bf25e
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-03 22:17:31 +0300

    Added solution

---
+++ 01. Python Basics/04. For Loop/4.2. For Loop - Exercise/08. Tennis Ranklist.py
@@ -0,0 +1,29 @@
+tournaments_count = int(input())
+starting_points = int(input())
+
+total_points = starting_points
+
+average_points = 0
+tournamets_won_average = 0
+
+for i in range(tournaments_count):
+    stage = input()
+    
+    if stage == 'W':
+        total_points += 2000
+        average_points += 2000
+        tournamets_won_average += 1
+    elif stage == 'F':
+        total_points += 1200
+        average_points += 1200
+    elif stage == 'SF':
+        total_points += 720
+        average_points += 720
+        
+average_points /= tournaments_count
+
+tournamets_won_average = (tournamets_won_average / tournaments_count) * 100
+
+print(f'Final points: {total_points}')
+print(f'Average points: {int(average_points)}')
+print(f'{tournamets_won_average:.2f}%')
