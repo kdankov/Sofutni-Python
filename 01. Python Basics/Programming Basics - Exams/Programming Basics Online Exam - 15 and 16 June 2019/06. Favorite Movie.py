61af9f77bff32064a7c495385d4cdcb2fd42ed56
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-12 17:58:57 +0300

    Added solution

---
+++ 01. Python Basics/Programming Basics - Exams/Programming Basics Online Exam - 15 and 16 June 2019/06. Favorite Movie.py
@@ -0,0 +1,34 @@
+movie_name = input()
+
+count = 0
+
+best_movie = {
+    'name': '',
+    'ascii_sum': 0
+}
+
+while movie_name != 'STOP':
+    current_sum = 0
+    for letter in movie_name:
+        if letter.islower():
+            current_sum = current_sum + (ord(letter) - len(movie_name) * 2)
+        elif letter.isupper():
+            current_sum = current_sum + (ord(letter) - len(movie_name))
+        else:
+            current_sum += ord(letter)
+    
+    if best_movie['ascii_sum'] < current_sum:
+        best_movie['name'] = movie_name
+        best_movie['ascii_sum'] = current_sum
+    
+    count += 1
+    
+    if count == 7:
+        break
+    
+    movie_name = input()
+    
+if count == 7:
+    print('The limit is reached.')
+
+print(f'The best movie for you is {best_movie["name"]} with {best_movie["ascii_sum"]} ASCII sum.')
