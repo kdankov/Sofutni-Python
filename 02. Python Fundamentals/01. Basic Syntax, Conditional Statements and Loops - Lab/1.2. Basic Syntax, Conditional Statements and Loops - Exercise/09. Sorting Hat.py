cd7908e4e701f85a34820da44add0ea3ec4d3b2b
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-20 20:35:53 +0300

    Added solution

---
+++ 02. Python Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/1.2. Basic Syntax, Conditional Statements and Loops - Exercise/09. Sorting Hat.py
@@ -0,0 +1,24 @@
+name = input()
+
+flag = False
+
+while name != 'Welcome!':
+    if name == 'Voldemort':
+        flag = True
+        break
+    
+    if len(name) < 5:
+        print(f'{name} goes to Gryffindor.')
+    elif len(name) == 5:
+        print(f'{name} goes to Slytherin.')
+    elif len(name) == 6:
+        print(f'{name} goes to Ravenclaw.')
+    else:
+        print(f'{name} goes to Hufflepuff.')
+    
+    name = input()
+    
+if flag:
+    print('You must not speak of that name!')
+else:
+    print('Welcome to Hogwarts.')
