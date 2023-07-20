8f7280c966e95a8a224588acfffb0df7105c7b79
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-20 19:10:35 +0300

    Added solution

---
+++ 02. Python Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/1.2. Basic Syntax, Conditional Statements and Loops - Exercise/03. Chat Codes.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    number = float(input())
+    if number == 88:
+        print('Hello')
+    elif number == 86:
+        print('How are you?')
+    elif number < 88 and number != 88 and number != 86:
+        print('GREAT!')
+    elif number > 88:
+        print('Bye.')
+    
+    if i == 3:
+        break
+
