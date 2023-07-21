c88300930988c0206965bc12998a64a857ebeeb5
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-21 15:49:26 +0300

    Added solution

--- 02. Python Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/1.3. Basic Syntax, Conditional Statements and Loops - More Exercises/03. Wolf In Sheep's Clothing.py
+++ 02. Python Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/1.3. Basic Syntax, Conditional Statements and Loops - More Exercises/03. Wolf In Sheep's Clothing.py
@@ -1,7 +1,11 @@
 animals_queue = input().split(', ')
-
-if animals_queue[-1] == 'sheep':
-    print(f'Oi! Sheep number {len(animals_queue) - 1}! You are about to be eaten by a wolf!')
-else:
-    print('Please go away and stop eating my sheep')
-
+ 
+if animals_queue.pop() == "wolf":
+    print("Please go away and stop eating my sheep")
+    raise SystemExit
+ 
+reversed_queue = animals_queue[::-1]
+ 
+for i in range(len(reversed_queue)):
+    if reversed_queue[i] != "sheep":
+        print(f"Oi! Sheep number {i + 1}! You are about to be eaten by a wolf!")
