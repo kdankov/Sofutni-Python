8568381c13a2ec32c51215b46d7eca85a1847e1d
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-21 16:00:48 +0300

    Added solution

---
+++ 02. Python Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/1.3. Basic Syntax, Conditional Statements and Loops - More Exercises/04. Sum Of A Beach.py
@@ -0,0 +1,5 @@
+initial_string = input().lower()
+
+count = initial_string.count('sand') + initial_string.count('water') + initial_string.count('fish') + initial_string.count('sun')
+        
+print(count)
