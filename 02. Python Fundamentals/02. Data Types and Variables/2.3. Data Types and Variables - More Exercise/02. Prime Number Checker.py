899eff672b438ae3b4fd77366e87888d906238f0
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-21 20:17:26 +0300

    Added solution

---
+++ 02. Python Fundamentals/02. Data Types and Variables/2.3. Data Types and Variables - More Exercise/02. Prime Number Checker.py
@@ -0,0 +1,13 @@
+number = int(input())
+
+is_prime = True
+
+if number == 1:
+    is_prime = False
+elif number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            is_prime = False
+            break
+
+print(is_prime)
