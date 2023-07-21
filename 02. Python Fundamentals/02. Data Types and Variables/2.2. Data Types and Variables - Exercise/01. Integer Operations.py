9320211790427547ced6453e5a8a55d2209057e7
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-21 19:30:51 +0300

    Added solution

---
+++ 02. Python Fundamentals/02. Data Types and Variables/2.2. Data Types and Variables - Exercise/01. Integer Operations.py
@@ -0,0 +1,10 @@
+from math import floor
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+fourth_num = int(input())
+
+result = floor((first_num + second_num) / third_num) * fourth_num
+
+print(result)
