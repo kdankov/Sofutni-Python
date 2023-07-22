aeb7d66ed4888fa075e4ec0697a59a7331cd88db
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-22 11:49:10 +0300

    Added solution

--- 02. Python Fundamentals/03. Lists Basics/3.2. Lists Basics - Exercise/07. Easter Gifts.py
+++ 02. Python Fundamentals/03. Lists Basics/3.2. Lists Basics - Exercise/07. Easter Gifts.py
@@ -8,12 +8,10 @@ while command != 'No Money':
     current_gift = tokens[1]
     
     if action == 'OutOfStock':
-        for i in range(len(gifts_to_buy)):
-            if gifts_to_buy[i] == current_gift:
-                gifts_to_buy[i] = 'None'
+        gifts_to_buy = ['None' if gift == current_gift else gift for gift in gifts_to_buy]
     elif action == 'Required':
         index = int(tokens[2])
-        if 0 < index < len(gifts_to_buy) + 1:
+        if 0 <= index <= len(gifts_to_buy):
             gifts_to_buy[index] = current_gift
     elif action == 'JustInCase':
         gifts_to_buy[-1] = current_gift
