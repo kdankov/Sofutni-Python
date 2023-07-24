04fc212a14dd17459c767af8ef00c2a4a1d3ecd1
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-07-24 21:32:08 +0300

    Added solution

---
+++ 02. Python Fundamentals/04. Functions/4.2. Functions - Exercise/09. Password Validator.py
@@ -0,0 +1,28 @@
+def is_digit(num):
+    return num >= 48 and num <= 57
+
+def num_count_minimum(password):
+    return len([x for x in password if x.isdigit()]) >= 2
+        
+
+def length_validator(password):
+        return 6 <= len(password) <= 10
+
+
+def is_letter_digit(password):
+    return password.isalnum()
+
+
+password = input()
+
+if length_validator(password) and is_letter_digit(password) and num_count_minimum(password):
+    print('Password is valid')
+else:
+    if not length_validator(password):
+        print('Password must be between 6 and 10 characters')
+        
+    if not is_letter_digit(password):
+        print('Password must consist only of letters and digits');
+
+    if not num_count_minimum(password):
+        print('Password must have at least 2 digits')
