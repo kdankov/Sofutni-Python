cd165026de26e173c024d8f766c8506f40e30a34
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-09-01 18:10:43 +0300

    Added solution

---
+++ 02. Python Fundamentals/08. Text Processing/8.3. Text Processing - More Exercises/04. Morse Code Translator.py
@@ -0,0 +1,18 @@
+code = input().split(' | ')
+
+morse_alphabet = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+
+result = ''
+for word in code:
+    args = word.split()
+    
+    for ch in args:    
+        current_letter_index = morse_alphabet.index(ch)
+        
+        current_letter_char = chr(current_letter_index + 97).upper()
+        
+        result += current_letter_char
+    
+    result += ' '
+    
+print(result)
