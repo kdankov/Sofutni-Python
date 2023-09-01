9bf13cef5a4984b4a2ac2510086cd2f7d1caedc3
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-09-01 17:41:04 +0300

    Added solution

---
+++ 02. Python Fundamentals/08. Text Processing/8.3. Text Processing - More Exercises/03. Treasure Finder.py
@@ -0,0 +1,30 @@
+key = input().split()
+
+current_line = input()
+
+while current_line != 'find':
+    decrypted = ''
+    count = 0
+
+    while count < len(current_line):
+        for j in range(len(key)):
+            if count == len(current_line):
+                break
+
+            current_number = int(key[j])
+            current_char = ord(current_line[count])
+            count += 1
+            decrypted_char = chr(current_char - current_number)
+            decrypted += decrypted_char 
+
+    first_index_type = decrypted.index('&')
+    last_index_type = decrypted.rindex('&')
+    type_treasure = decrypted[first_index_type + 1: last_index_type]
+    
+    first_index_coordinates = decrypted.index('<')
+    last_index_coordinates = decrypted.rindex('>')
+    coordinates = decrypted[first_index_coordinates + 1: last_index_coordinates]
+    
+    print(f'Found {type_treasure} at {coordinates}')
+    
+    current_line = input()
