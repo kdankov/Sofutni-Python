df4d5a390565972a9b4ad9094b06b625f9c15973
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-09-01 18:16:04 +0300

    Added solution

--- 02. Python Fundamentals/08. Text Processing/8.3. Text Processing - More Exercises/04. Morse Code Translator.py
+++ 02. Python Fundamentals/08. Text Processing/8.3. Text Processing - More Exercises/04. Morse Code Translator.py
@@ -6,7 +6,7 @@ result = ''
 for word in code:
     args = word.split()
     
-    for ch in args:    
+    for ch in args:
         current_letter_index = morse_alphabet.index(ch)
         
         current_letter_char = chr(current_letter_index + 97).upper()
