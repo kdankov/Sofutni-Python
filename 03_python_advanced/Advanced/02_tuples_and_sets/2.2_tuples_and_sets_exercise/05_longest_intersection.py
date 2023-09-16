d84c42a7c3884ed663219bdbd4ff5d18e6169497
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-09-16 22:02:54 +0300

    Added solution

--- 03_python_advanced/Advanced/02_tuples_and_sets/2.2_tuples_and_sets_exercise/05_longest_intersection.py
+++ 03_python_advanced/Advanced/02_tuples_and_sets/2.2_tuples_and_sets_exercise/05_longest_intersection.py
@@ -4,10 +4,6 @@ longest_intersection = set()
 
 for _ in range(n):
     data = input().split('-')
-
-    first_set = set()
-    second_set = set()
-
     first_intersection_range = data[0].split(',')
     first_start = int(first_intersection_range[0])
     first_end = int(first_intersection_range[1])
@@ -16,14 +12,7 @@ for _ in range(n):
     second_start = int(second_intersection_range[0])
     second_end = int(second_intersection_range[1])
 
-    for i in range(first_start, first_end + 1):
-        first_set.add(i)
-
-    for i in range(second_start, second_end + 1):
-        second_set.add(i)
-
-    current_intersection = first_set & second_set
-
+    current_intersection = set(range(first_start, first_end + 1)) & set(range(second_start, second_end + 1))
     if len(current_intersection) > len(longest_intersection):
         longest_intersection = current_intersection
 
