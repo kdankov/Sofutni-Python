0a0191f83f81e4fb4e5022efff5a95f4a83a2b7f
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-09-21 16:04:26 +0300

    Added solution

--- 03_python_advanced/Advanced/04_multidimensional_lists/4.1_multidimensional_lists_lab/01_sum_matrix_elements.py
+++ 03_python_advanced/Advanced/04_multidimensional_lists/4.1_multidimensional_lists_lab/01_sum_matrix_elements.py
@@ -1,14 +1,12 @@
 rows, cols = [int(x) for x in input().split(", ")]
 matrix = []
 
-for row in range(rows):
-    lines = [int(x) for x in input().split(", ")]
-    matrix.append(lines)
-
 sum_of_all_numbers = 0
 
-for el in matrix:
-    sum_of_all_numbers += sum(el)
+for row in range(rows):
+    lines = [int(x) for x in input().split(", ")]
+    sum_of_all_numbers += sum(lines)
+    matrix.append(lines)
 
 print(sum_of_all_numbers)
 print(matrix)
