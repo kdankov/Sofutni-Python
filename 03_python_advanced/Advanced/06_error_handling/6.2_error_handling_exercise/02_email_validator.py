4fde61ddbfad93b9e415c0dfe74d55208152b6a9
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-10-02 20:38:09 +0300

    Added solution

---
+++ 03_python_advanced/Advanced/06_error_handling/6.2_error_handling_exercise/02_email_validator.py
@@ -0,0 +1,31 @@
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class NameTooShortError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+email = input()
+
+EMAIL_MIN_LENGTH = 5
+VALID_DOMAINS = ['.com', '.bg', '.org', '.net']
+
+while email != 'End':
+    if '@' not in email:
+        raise MustContainAtSymbolError('Email must contain @')
+
+    if len(email.split('@')[0]) < EMAIL_MIN_LENGTH:
+        raise NameTooShortError('Name must be more than 4 characters')
+
+    domain = '.' + email.split('.')[-1]
+    if domain not in VALID_DOMAINS:
+        raise InvalidDomainError('Domain must be one of the following: .com, .bg, .org, .net')
+
+    print('Email is valid')
+
+    email = input()
