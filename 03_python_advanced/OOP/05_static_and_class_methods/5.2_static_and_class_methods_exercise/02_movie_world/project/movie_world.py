ce1ef2b98faa497ccbd52cbbc2c593fe63a1c53b
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-11-07 00:18:15 +0200

    Added solution

---
+++ 03_python_advanced/OOP/05_static_and_class_methods/5.2_static_and_class_methods_exercise/02_movie_world/project/movie_world.py
@@ -0,0 +1,72 @@
+from typing import List
+from project.dvd import DVD
+from project.customer import Customer
+
+
+class MovieWorld:
+    def __init__(self, name: str):
+        self.name = name
+        self.customers: List[Customer] = []
+        self.dvds: List[DVD] = []
+
+    @staticmethod
+    def dvd_capacity() -> int:
+        return 15
+
+    @staticmethod
+    def customer_capacity() -> int:
+        return 10
+
+    def __get_customer_by_id(self, customer_id) -> Customer:
+        for customer in self.customers:
+            if customer_id == customer.id:
+                return customer
+
+    def __get_dvd_by_id(self, dvd_id) -> DVD:
+        for dvd in self.dvds:
+            if dvd.id == dvd_id:
+                return dvd
+
+    def add_customer(self, customer: Customer) -> None:
+        if len(self.customers) < self.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD) -> None:
+        if len(self.dvds) < self.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id: int, dvd_id: int) -> str:
+        customer = self.__get_customer_by_id(customer_id)
+        dvd = self.__get_dvd_by_id(dvd_id)
+
+        if dvd in customer.rented_dvds:
+            return f'{customer.name} has already rented {dvd.name}'
+
+        if dvd.is_rented:
+            return 'DVD is already rented'
+
+        if customer.age < dvd.age_restriction:
+            return f'{customer.name} should be at least {dvd.age_restriction} to rent this movie'
+
+        customer.rented_dvds.append(dvd)
+        dvd.is_rented = True
+        return f'{customer.name} has successfully rented {dvd.name}'
+
+    def return_dvd(self, customer_id: int, dvd_id: int) -> str:
+        customer = self.__get_customer_by_id(customer_id)
+        dvd = self.__get_dvd_by_id(dvd_id)
+
+        if dvd not in customer.rented_dvds:
+            return f'{customer.name} does not have that DVD'
+
+        customer.rented_dvds.remove(dvd)
+        dvd.is_rented = False
+        return f'{customer.name} has successfully returned {dvd.name}'
+
+    def __repr__(self):
+        result = ""
+        for customer in self.customers:
+            result += f"{customer.__repr__()}\n"
+        for dvd in self.dvds:
+            result += f"{dvd.__repr__()}\n"
+        return result
