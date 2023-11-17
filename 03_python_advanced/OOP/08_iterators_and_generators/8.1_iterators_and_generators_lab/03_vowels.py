9014a99f529462a778cbcc3458ac0ecb0901b64d
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-11-17 19:16:17 +0200

    Added solution

---
+++ 03_python_advanced/OOP/08_iterators_and_generators/8.1_iterators_and_generators_lab/03_vowels.py
@@ -0,0 +1,24 @@
+class vowels:
+    def __init__(self, text: str):
+        self.text = text
+        vowels_const = ['a', 'i', 'e', 'u', 'y', 'o']
+        self.found_vowels = [char for char in self.text if char.lower() in vowels_const]
+        self.curr_index = 0
+        self.end_index = len(self.found_vowels) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.curr_index > self.end_index:
+            raise StopIteration()
+
+        index = self.curr_index
+        self.curr_index += 1
+
+        return self.found_vowels[index]
+
+
+my_string = vowels('Abcedifuty0o')
+for char in my_string:
+    print(char)
