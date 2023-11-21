dabc58f8758fe8ef049bc9bd7e3a411a25f6848e
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-11-21 18:18:02 +0200

    Added solution

--- 03_python_advanced/OOP/09_decorators/9.1_decorators_lab/01_number_increment.py
+++ 03_python_advanced/OOP/09_decorators/9.1_decorators_lab/01_number_increment.py
@@ -1,7 +1,7 @@
 def number_increment(numbers):
     def increase():
-        increased = [x + 1 for x in numbers]
-        return increased
+        return [x + 1 for x in numbers]
+
     return increase()
 
 
